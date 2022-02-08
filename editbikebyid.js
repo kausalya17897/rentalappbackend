1b63707e329cc9fc946a0a09857424dae13a2d2a
Kausalya k <kausalya17897@gmail.com>
2022-02-08 17:03:29 +0530

    id to _id

--- editbikebyid.js
+++ editbikebyid.js
@@ -1,10 +1,11 @@
+import { ObjectId } from 'mongodb';
 import { client } from './index.js';
 
 
 export async function editbikebyid(id, data) {
   return await client.db("bikerental")
     .collection("users")
-    .updateOne({ id: id }, { $set: data });
+    .updateOne({ _id: id }, { $set: data });
 }
 export async function getbike() {
   return await client
@@ -16,13 +17,14 @@ export async function deletebikebyid(id) {
   return await client
     .db("bikerental")
     .collection("users")
-    .deleteOne({ id: id });
+    .deleteOne({ _id: id });
 }
 export async function getbikebyid(id) {
+  console.log("***",id)
   return await client
     .db("bikerental")
     .collection("users")
-    .findOne({ id: id });
+    .findOne({ _id:ObjectId(id)});
 }
 export async function updateBike(data) {
   return await client
