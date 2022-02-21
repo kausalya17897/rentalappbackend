ebe7c64095f4d7f873f52604b49316b9ebad249b
Kausalya.K <85003124+kausalya17897@users.noreply.github.com>
2022-02-21 11:54:30 +0530

    dot

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ import {genPassword} from "../editbikebyid.js";
 import bcrypt from 'bcrypt'
 import jwt  from 'jsonwebtoken';
 const router=express.Router();
+dotenv.config();
 router
 .route("/signup").post(async(request,response)=>{
   const {username,password}=request.body;
